f08f50089b95c4e2d8b4a695eb1009627296b1b6
Lucas Rachid Martins <lucasrachid@hotmail.com>
2024-07-09 15:10:37 -0300

    Classe e função para inverter uma árvore binária.

--- MyPlayground.playground/Pages/classes.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/classes.xcplaygroundpage/Contents.swift
@@ -71,3 +71,53 @@ class PersonAux {
         self.lastName = lastName
     }
 }
+
+
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init() { self.val = 0; self.left = nil; self.right = nil; }
+    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil }
+    
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+}
+
+public class Solution {
+    
+    func invertTree(_ root: TreeNode?) -> TreeNode? {
+        guard let root else { return nil }
+        let left = invertTree(root.left)
+        let right = invertTree(root.right)
+        root.right = left
+        root.left = right
+        
+        return root
+    }
+    
+    func printTree(_ root: TreeNode?) {
+        guard let root = root else { return }
+        print(root.val)
+        printTree(root.left)
+        printTree(root.right)
+    }
+    
+}
+
+
+
+let inputRoot: TreeNode = TreeNode(2)
+inputRoot.left = TreeNode(1)
+inputRoot.right = TreeNode(3)
+
+let solution = Solution()
+
+if let invertedRoot = solution.invertTree(inputRoot) {
+    solution.printTree(invertedRoot)
+}
+
+
